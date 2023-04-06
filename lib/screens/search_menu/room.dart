27689e5041c0c7b2eb96eb9eeed3ee6eec2e47ee
jishajose492 <jishajose492@gmail.com>
2023-04-06 18:23:23 +0530

    third changes

---
+++ lib/screens/search_menu/room.dart
@@ -0,0 +1,13 @@
+class Room {
+  final int roomNumber;
+  final String roomType;
+  final int numberOfBeds;
+  final bool isAvailable;
+
+  Room({
+    required this.roomNumber,
+    required this.roomType,
+    required this.numberOfBeds,
+    required this.isAvailable,
+  });
+}
